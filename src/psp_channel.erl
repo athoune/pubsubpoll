b191bcdd92e36b28a9b6656c60ba94cca29306ca
Mathieu Lecarme <mathieu@garambrogne.net>
2011-07-05 23:16:13 +0200

    You can publish directly to a channel

--- src/psp_channel.erl
+++ src/psp_channel.erl
@@ -7,7 +7,7 @@
 -export([start_link/3, init/1, handle_call/3, handle_cast/2, 
 handle_info/2, terminate/2, code_change/3]).
 
--export([filter/1]).
+-export([filter/1, publish/2]).
 
 -record(state, {
     name,
@@ -130,6 +130,9 @@ code_change(_OldVsn, State, _Extra) ->
 
 filter(Channel) ->
     gen_server:call(Channel, filter).
+
+publish(Channel, Event) ->
+    gen_server:cast(Channel, {event, Event}).
 
 %%--------------------------------------------------------------------
 %% Private API
